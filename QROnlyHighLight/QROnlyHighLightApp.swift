64dcaded377256ac104d69350566b57aa394ef02
tbskgm0 <tsubasa.kogoma@axia-works.com>
2022-11-19 09:31:39 +0900

    Initial Commit

---
+++ QROnlyHighLight/QROnlyHighLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  QROnlyHighLightApp.swift
+//  QROnlyHighLight
+//
+//  Created by tsubasa.kogoma on 2022/11/19.
+//
+
+import SwiftUI
+
+@main
+struct QROnlyHighLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
